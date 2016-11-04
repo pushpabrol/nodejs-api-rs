2ef6f609fab402c68f14aa4ac98a0c70acb51768
pushpabrol <pushp.abrol@gmail.com>
2016-11-04 13:37:22 -0400

    Node API - Token Algorithm RS256
    Signed-off-by: pushpabrol <pushp.abrol@gmail.com>

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
+var jwt = require('express-jwt');
+var jwksRsa = require('jwks-rsa');
+var cors = require('cors');
+var http = require('http');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var router = express.Router();
+
+dotenv.load();
+
+var authenticate = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://' + process.env.AUTH0_DOMAIN + '/.well-known/jwks.json'
+  }),
+
+
+  // Validate the audience and the issuer
+  audience: process.env.AUDIENCE,
+  issuer: 'https://' + process.env.AUTH0_DOMAIN + '/',
+  algorithms: ['RS256']
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({
+      message: 'Missing or invalid token'
+    });
+  }
+});
+
+app.use(cors());
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/secured', authenticate);
+
+
+app.get('/ping', function(req, res) {
+  res.send("All good. You don't need to be authenticated to call this");
+});
+
+
+app.get('/secured/ping', function(req, res) {
+  res.status(200).send("All good. You only get this message if you're authenticated");
+});
+
+var port = process.env.PORT || 3001;
+
+http.createServer(app).listen(port, function(err) {
+  console.log('listening in http://localhost:' + port);
+});
+
+module.exports = app;
